043f48a7d9df1660950f0bb13a9f6a0e9d2e24ec
Sergo Tsoko <cokolenko.sergo@gmail.com>
2025-02-06 10:04:57 +0200

    first

--- JS/main.js
+++ JS/main.js
@@ -3,13 +3,26 @@ document.querySelector(".burger").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".nav-list-mob").classList.toggle("open");
   document.querySelector("body").classList.toggle("no-scroll");
+  document.querySelector("body").classList.toggle("bg-color");
+
+
 });
 
 document.querySelector(".nav-list-mob").addEventListener("click", function () {
   this.classList.toggle("open");
   document.querySelector("body").classList.toggle("no-scroll");
+  document.querySelector("body").classList.toggle("bg-color");
+
   document.querySelector(".burger").classList.toggle("active");
 });
+document.querySelector(".closer").addEventListener("click", function () {
+  this.classList.toggle("open1");
+  document.querySelector("body").classList.toggle("no-scroll");
+  document.querySelector(".nav-list-mob").classList.toggle("open");
+
+  document.querySelector(".burger").classList.toggle("active");
+});
+
 
 
 let swiper = new Swiper(".mySwiper", {
