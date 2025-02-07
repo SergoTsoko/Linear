6c175bd108fd095bcbd7956ac22a2a06ef290607
Sergo Tsoko <cokolenko.sergo@gmail.com>
2025-02-07 11:39:43 +0200

    first

--- JS/main.js
+++ JS/main.js
@@ -15,42 +15,44 @@ document.querySelector(".nav-list-mob").addEventListener("click", function () {
 
   document.querySelector(".burger").classList.toggle("active");
 });
-document.querySelector(".closer").addEventListener("click", function () {
-  this.classList.toggle("open1");
-  document.querySelector("body").classList.toggle("no-scroll");
-  document.querySelector(".nav-list-mob").classList.toggle("open");
+// document.querySelector(".closer").addEventListener("click", function () {
+//   this.classList.toggle("open1");
+//   document.querySelector("body").classList.toggle("no-scroll");
+//   document.querySelector(".nav-list-mob").classList.toggle("open");
 
-  document.querySelector(".burger").classList.toggle("active");
-});
+//   document.querySelector(".burger").classList.toggle("active");
+// });
 
 
 
-let swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 1,
+  initialSlide: 0,
  
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    // loop: true,
+    el: ".swiper-scrollbar",
+
   },
+  
  
-  breakpoints: {
-    500: {
-      slidesPerView: 3,
-      spaceBetween: 1,
-    },
-  },
-  breakpoints: {
-    400: {
-      slidesPerView: 2,
-      spaceBetween: 1,
-    },
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 1,
-    },
-  },
+  // breakpoints: {
+  //   500: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 1,
+  //   },
+  // },
+  // breakpoints: {
+  //   400: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 1,
+  //   },
+  // },
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 1,
+  //   },
+  // },
 });
+
